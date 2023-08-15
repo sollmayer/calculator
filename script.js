591b2945c53a69a22b3e1784429773c9c81933f9
Serhii <stupanenko.serhii@gmail.com>
2023-08-15 23:00:33 +0300

    create operate and populateDisplay functions

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ function subtract(a,b) {return a - b}
 function multiply(a,b) {return a * b}
 function divide(a,b) {return a / b}
 
+let operator = '';
+let firstNumber = 0;
+let secondNumber = 0;
 
+function operate(operator, firstNumber, secondNumber) {
+    if(operator === "+") return add(firstNumber, secondNumber);
+    else if(operator === "-") return subtract(firstNumber, secondNumber);
+    else if(operator === "*") return multiply(firstNumber, secondNumber);
+    else if(operator === "/") return divide(firstNumber, secondNumber);
+}
+
+function populateDisplay(item){
+    let display = document.querySelector('.display p');
+    display.textContent += `${item}`
+}
