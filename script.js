409bcb7fd417f5105d7cd6a9cae8c7ecebe98112
Serhii <stupanenko.serhii@gmail.com>
2023-08-16 16:37:12 +0300

    add functions to handle click of CLEAR and RESULT

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function add(a,b) {return a + b}
+function add(a,b) {return Number(a) + Number(b)}
 function subtract(a,b) {return a - b}
 function multiply(a,b) {return a * b}
 function divide(a,b) {return a / b}
 
 let operator = null;
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNumber = '';
+let secondNumber = '';
 
 function operate(operator, firstNumber, secondNumber) {
     if(operator === "+") return add(firstNumber, secondNumber);
@@ -25,16 +25,38 @@ function populateScreen(item){
     screen.textContent += item
 }
 
-numbers.forEach(number => number.addEventListener('click', ()=>populateScreen(number.textContent)))
+numbers.forEach(number => number.addEventListener('click', ()=>handleNumberClick(number.textContent)))
 operation.forEach(operator => operator.addEventListener('click', ()=>setOperation(operator.textContent)))
+clearBtn.addEventListener('click',  clearScreen)
+resultBtn.addEventListener('click', ()=>{
+    const result = operate(operator, firstNumber, secondNumber);
+    clearScreen()
+    populateScreen(result)
+    firstNumber = screen.textContent;
+})
 
+function handleNumberClick(number){
+    if(operator !== null) {secondNumber += number}
+    else firstNumber += number;
+
+    console.log('firstNumber', firstNumber);
+    console.log('secondNumber', secondNumber);
+    populateScreen(number);
+}
 function setOperation(operation) {
-    if(operator === operation) return;
-    operator = operation;
-    console.log(operator);
+    if(firstNumber !== '' && secondNumber !== '') return;
+    if(firstNumber !== '') {
+        if(operator === operation) return;
+        operator === null ? populateScreen(operation) : screen.textContent = screen.textContent.slice(0,-1) + operation;
+        operator = operation;
+        console.log(operator);
+    }
+    
 }
 
-function clearDisplay() {
-    firstNumber = 0;
-    document.querySelector('.display p').textContent = '';
+function clearScreen() {
+    firstNumber = '';
+    secondNumber = '';
+    operator = null;
+    screen.textContent = '';
 }
