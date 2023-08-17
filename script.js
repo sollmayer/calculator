f485164e1b501b887bf080922a5d060652c9ffc5
Serhii <stupanenko.serhii@gmail.com>
2023-08-17 22:40:33 +0300

    implement delete button logic and opportunity to use decimals

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function divide(a,b) {return a / b}
 let operator = null;
 let firstNumber = '';
 let secondNumber = '';
+let decimalSelected = false;
 
 function operate(operator, firstNumber, secondNumber) {
     if(operator === "+") return add(firstNumber, secondNumber);
@@ -20,35 +21,71 @@ const numbers = document.querySelectorAll(".digit");
 const operation = document.querySelectorAll(".operation");
 const clearBtn = document.querySelector('.clearBtn');
 const resultBtn = document.querySelector('.resultBtn');
+const decimalBtn = document.querySelector('.decimal');
+const deleteBtn = document.querySelector('.deleteBtn');
+
+numbers.forEach(number => number.addEventListener('click', ()=>handleNumberClick(number.textContent)));
+operation.forEach(operator => operator.addEventListener('click', ()=>setOperation(operator.textContent)));
+clearBtn.addEventListener('click',  clearScreen);
+resultBtn.addEventListener('click', handleResultBtnClick);
+decimalBtn.addEventListener('click', handleDecimalBtnClick);
+deleteBtn.addEventListener('click', handleDeleteBtnClick);
+
 
 function populateScreen(item){
     screen.textContent += item
 }
 
-numbers.forEach(number => number.addEventListener('click', ()=>handleNumberClick(number.textContent)))
-operation.forEach(operator => operator.addEventListener('click', ()=>setOperation(operator.textContent)))
-clearBtn.addEventListener('click',  clearScreen)
-resultBtn.addEventListener('click', ()=>{
-    const result = operate(operator, firstNumber, secondNumber);
-    clearScreen()
-    populateScreen(result)
-    firstNumber = screen.textContent;
-})
-
 function handleNumberClick(number){
     if(operator !== null) {secondNumber += number}
     else firstNumber += number;
-
     console.log('firstNumber', firstNumber);
     console.log('secondNumber', secondNumber);
     populateScreen(number);
 }
+
+function handleResultBtnClick(){
+    if(firstNumber !== '' && secondNumber !== ''){
+        const result = operate(operator, firstNumber, secondNumber);
+        clearScreen()
+        populateScreen(result)
+        firstNumber = screen.textContent;
+    }
+}
+
+function handleDecimalBtnClick() {
+    const prevChar = screen.textContent[screen.textContent.length - 1];
+
+    if(!isNaN(prevChar) && !decimalSelected){ 
+        populateScreen('.');
+        decimalSelected = true;
+        if(operator === null) parseFloat(firstNumber += '.')
+        else if (operator !== null) parseFloat(secondNumber += '.')
+    }
+    console.log(prevChar)
+}
+
+function handleDeleteBtnClick() {
+    const prevCharacter = screen.textContent.slice(0,-1);
+    screen.textContent = prevCharacter;
+    if(operator === null && secondNumber === '') {
+        firstNumber = screen.textContent;
+    }else if(operator !== null && secondNumber === '') {
+        operator = null;
+    }else if(firstNumber !== '' && operator !== null){
+        let operatorInd = screen.textContent.split('').findIndex(item => item === operator);
+        secondNumber = screen.textContent.slice(operatorInd+1);
+        console.log(secondNumber)
+    }
+}
+
 function setOperation(operation) {
-    if(firstNumber !== '' && secondNumber !== '') return;
+    if(firstNumber !== '' && secondNumber !== '') handleResultBtnClick();
     if(firstNumber !== '') {
         if(operator === operation) return;
         operator === null ? populateScreen(operation) : screen.textContent = screen.textContent.slice(0,-1) + operation;
         operator = operation;
+        decimalSelected = false;
         console.log(operator);
     }
     
@@ -58,5 +95,6 @@ function clearScreen() {
     firstNumber = '';
     secondNumber = '';
     operator = null;
+    decimalSelected = false;
     screen.textContent = '';
 }
